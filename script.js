c0c9e412230261e1f63f362840b4208fb4d923ea
Luiz3264 <luizfelipe20043264@gmail.com>
2026-03-03 21:09:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function load(){
+    text = document.getElementById("text");
+    calc = document.getElementById("calc");
+}
+
+function calculate(){
+    try {
+        var result = eval(calc.value);
+    } 
+    catch (e) {
+        text.innerHTML='error'
+    }
+    text.innerText = result;
+}
